0be83bcba966c088d60c479ea79d0f1115fffdfd
Hernan H <hernanlitio@gmail.com>
2023-01-25 09:50:53 -0300

    se agrego el modulo de login

--- graphQL/resolvers/resolvers.js
+++ graphQL/resolvers/resolvers.js
@@ -1,3 +1,41 @@
+const bcryptjs = require('bcryptjs')
+const jose = require('jose')
+
+const formatErrors = (error,otherErrors)=>{
+    const errors=error.errors;
+    let objErrors = []
+  
+    if(errors){
+      Object.entries(errors).map(error=>{
+        const {path, message} = error[1];
+        objErrors.push({path,message})
+      })
+      objErrors = objErrors.concat(otherErrors)
+      return objErrors;
+    }else if(otherErrors.length){
+      return otherErrors;
+    }
+  
+  
+    const uknownError = {}
+    switch(error.code){
+      case 11000:
+        uknownError.path = "username"
+        uknownError.message = "El nombre de usuario ya existe"
+      break;
+      default:
+        uknownError.path = "Desconocido"
+        uknownError.message = error.message
+    }
+    return [uknownError]
+  
+  }
+
+
+
+
+
+
 const resolvers = {
     Query:{
         async getTrabajos( root, args, { models}){
@@ -10,12 +48,87 @@ const resolvers = {
     },
 
     Mutation: {
+
+        login: async (root,{correo, password}, { models })=>{
+            
+            const usuario = await models.usuario.findOne(
+                {
+                    where:{correo:correo}
+                })
+
+            console.log(usuario)
+
+            if(!usuario){
+                return {
+                  success:false,
+                  errors:[{path:'correo', message:'Correo no existe'}]
+                }
+              }
+
+              const validPassword = await bcryptjs.compare(password, usuario.password)
+              if(!validPassword){
+                return {
+                  success:false,
+                  errors:[{path:'password', message:'Password inválido'}]
+                }
+              }
+          
+
+            const secret = new TextEncoder().encode(
+            'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
+            )
+            const alg = 'HS256'
+            
+            const jwt = await new jose.SignJWT({ user:123456 })
+            .setProtectedHeader({ alg })
+            .setIssuedAt()
+            .setIssuer('urn:example:issuer')
+            .setAudience('urn:example:audience')
+            .setExpirationTime('2h')
+            .sign(secret)
+
+            console.log("Token:", jwt)
+
+            
+              
+          
+            return {
+                success: true,
+                token:jwt,
+                errors: []
+              }
+        },
+
+
+
+
+
+        
+
         async createTrabajo(root,{imagen,imagen_alt,titulo,subtitulo,enlace},{ models }){
             return await models.portafolio.create ({imagen,imagen_alt,titulo,subtitulo,enlace})
         },
 
-        async createUsuario(root,{correo,password,idperfil},{ models }){
-            return await models.usuario.create ({correo,password,idperfil})
+        createUsuario: async (root,{correo,password,idperfil},{ models })=>{
+            const otherErrors = []
+            try{
+                let passencryp = await bcryptjs.hash(password,8)
+                console.log ("Token: ", passencryp)
+                
+                const usuariocreado = await models.usuario.create ({correo, password:passencryp,idperfil})
+                //console.log (usuariocreado)
+                return {
+                    success: usuariocreado && usuariocreado.idusuario,
+                    errors: []
+                  };
+                
+            }catch(error){
+                return {
+                    success: false,
+                    errors: formatErrors(error,otherErrors)
+                  };
+                
+            }
         }
     }
 
--- graphQL/typeDefs/typeDefs.js
+++ graphQL/typeDefs/typeDefs.js
@@ -1,6 +1,17 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Error{
+    path: String!
+    message: String!
+  }
+
+  type Response {
+    success: Boolean!
+    token: String
+    errors: [Error]
+  }
+
   type Portafolio {
     idportafolio: Int!,
     imagen: String!,
@@ -24,10 +35,12 @@ const typeDefs = gql`
   }
 
   type Mutation{
+
+    login(correo: String!, password: String!): Response!
     
     createTrabajo(imagen:String!, imagen_alt:String!, titulo: String!, subtitulo: String!, enlace: String!):Portafolio!
 
-    createUsuario( correo: String!, password: String!, idperfil: Int!):Usuario!
+    createUsuario( correo: String!, password: String!, idperfil: Int!):Response!
 
 
   }
